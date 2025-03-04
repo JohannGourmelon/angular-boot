208673072f54d1316693e63381e8916543689c80
JohannGourmelon <johann.gourmelon@gmail.com>
2025-03-04 14:45:33 +0100

    add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { CommonModule } from '@angular/common';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AddTutorialComponent, TutorialDetailsComponent, TutorialsListComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, CommonModule],
+  providers: [provideHttpClient()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
